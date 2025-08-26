75069a48759240b38e695dc0c464766f78439b91
sadriddinovtohir <toxir4626@gmail.com>
2025-08-26 18:14:24 +0500

    ...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,3 +3,9 @@ import React from "react";
 export default function Header() {
   return <div>header</div>;
 }
+
+
+// 1. git add .
+// 2. git commit -m "..."
+// 3. git pull origin main
+// 4. git push origin branch name
