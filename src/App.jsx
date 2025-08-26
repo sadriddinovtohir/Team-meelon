be9ac1e7c042db0315a41168ebadb14669088a51
sadriddinovtohir <toxir4626@gmail.com>
2025-08-26 18:05:00 +0500

    ...

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (<>
+      <h1 className="text-3xl font-bold underline text-blue-500">
+      Salom Tailwind 3.4.17!
+    </h1>
+    <div className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eni tempore, nostrum iste minus harum ratione quasi, est ducimus beatae deserunt laudantium quod quo ab illum similique aliquam perferendis? Aut?</div>
+</>
+  )
+}
+
+export default App
